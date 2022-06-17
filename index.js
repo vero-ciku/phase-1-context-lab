172ea53d85bd0b8a6739a27d34031b2dd28fb520
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-16 18:44:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(array){
+    let newArr = []
+    array.forEach(element =>{
+        newArr.push(createEmployeeRecord(element))
+    })
+    return newArr
+}
+
+function createTimeInEvent(timeIn){
+    let hour = parseInt(timeIn.split(" ")[1])
+    let date = timeIn.split(" ")[0]
+    this["timeInEvents"].push({
+        type: "TimeIn",
+        hour: hour,
+        date: date
+    })
+    return this;
+}
+function createTimeOutEvent(timeOut){
+    let hour = parseInt(timeOut.split(" ")[1])
+    let date = timeOut.split(" ")[0]
+    this["timeOutEvents"].push({
+        type: "TimeOut",
+        hour: hour,
+        date: date
+    })
+    return this;
+}
+function hoursWorkedOnDate( date){
+    let inEvent = this.timeInEvents.find(event => event.date === date)
+    let outEvent = this.timeOutEvents.find(event => event.date === date)
+    return (outEvent.hour - inEvent.hour) / 100
+}
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    const rate = this["payPerHour"]
+    return hoursWorked * rate
+}
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +72,7 @@ const allWagesFor = function () {
 
     return payable
 }
-
+function calculatePayroll(employeearr) {
+    return employeearr.reduce((memo, rec)=>
+        memo + allWagesFor.call(rec), 0)
+}
